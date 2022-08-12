f7c32a52087f7db3e4d07cb96e5296e3f67c1194
d1ng0ls <4b4c4t3666@gmail.com>
2022-08-12 10:49:16 -0300

    eduardo

--- includes/conexao.php
+++ includes/conexao.php
@@ -1,17 +1,3 @@
-<<<<<<< HEAD:includes/conexao.php
-<?php 
-    $hostname= "localhost";
-    $port = 3307; //3306 - home | 3307 - school
-    $username = "root";
-    $password = "";
-    $database = "cuprom";
-    $con = mysqli_connect($hostname, $username, $password, $database, $port);
-
-    if(mysqli_connect_errno()){
-        printf("Erro ao conectar ao banco de dados: %s\n<br><br>", mysqli_connect_error());
-        exit;
-    }
-=======
 <?php 
     function conecta() : mysqli
     {
@@ -42,5 +28,4 @@
     {
         mysqli_close($conexao);
     }
->>>>>>> aa80959b12cf2dc6bca81f14c43888019286d9f8:pages/conexao.php
 ?>
