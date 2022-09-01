67efc36617d7c5383d758e52ea4a5da66ea13747
d1ng0ls <4b4c4t3666@gmail.com>
2022-08-31 22:03:48 -0300

    eduardo

--- includes/inicio.php
+++ includes/inicio.php
@@ -2,46 +2,42 @@
 
 ?>
 <!DOCTYPE html>
-    <html lang="pt-br">
-        <head>
-            <meta charset="UTF-8">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <link rel="stylesheet" href="style/style.css">
-            <link rel="stylesheet" href="style/style_navegador.css">
-            <title>COMECOM</title>
-        </head>
-        <body>
-            <!-- Dentro das divs ficarão os carroséis-->
-            <div class="promocoes"> 
-            <h2>Melhores Ofertas:</h2>
-                <p>Olá, afroamericano feliz
-                Olá catapimbas, afroamericano feliz
-                Olá catapimbas, afroamericano feliz
-                </p>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style/style.css">
+        <link rel="stylesheet" href="style/style_navegador.css">
+        <title>COMECOM</title>
+    </head>
+    <body>
+        <!-- Dentro das divs ficarão os carroséis-->
+        <div class="promocoes"> 
+        <h2>Melhores Ofertas:</h2>
+            <p>Olá, afroamericano feliz
+            Olá catapimbas, afroamericano feliz
+            Olá catapimbas, afroamericano feliz
+            </p>
+        <hr class="separador">
+        </div>
 
-                
-            <hr class="separador">
-            </div>
-            <div class="promocoes">
-                <h2>Tempo limitado:</h2>
-            <p>Olá catapimbas, afroamericano feliz
-                Olá catapimbas, afroamericano feliz
-                Olá catapimbas, afroamericano feliz
-                </p>
+        <div class="promocoes">
+            <h2>Tempo limitado:</h2>
+        <p>Olá catapimbas, afroamericano feliz
+            Olá catapimbas, afroamericano feliz
+            Olá catapimbas, afroamericano feliz
+            </p>
+        <hr class="separador">
+        </div>
 
-                
-            <hr class="separador">
-            </div>
-            <div class="promocoes">
-            <h2>Nas proximidades:</h2>
-            <p>Olá catapimbas, afroamericano feliz
-                Olá catapimbas, afroamericano feliz
-                Olá catapimbas, afroamericano feliz
-                </p>
-
-                
-            <hr class="separador">
-            </div>
-        </body>
-    </html>
+        <div class="promocoes">
+        <h2>Nas proximidades:</h2>
+        <p>Olá catapimbas, afroamericano feliz
+            Olá catapimbas, afroamericano feliz
+            Olá catapimbas, afroamericano feliz
+            </p>  
+        <hr class="separador">
+        </div>
+    </body>
+</html>
