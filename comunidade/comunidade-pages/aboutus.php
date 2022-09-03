5c74cc6e5392f91f64124d5ffd99fdcafff6c803
d1ng0ls <4b4c4t3666@gmail.com>
2022-09-02 21:10:20 -0300

    eduardo

--- comunidade/comunidade-pages/aboutus.php
+++ comunidade/comunidade-pages/aboutus.php
@@ -10,8 +10,10 @@
     <link rel="stylesheet" href="../../style/style_navegador.css">
     <link rel="stylesheet" href="../../style/style_comunidade.css">
     <title>COMECOM | Comunidade</title>
-    
 </head>
+
+<script src="../../includes/githubapi.js"></script>
+<script>github();github2();github3();github4();github5();</script>
 
 <body>
     <?php include('../../includes/navigator.php'); ?>
@@ -22,44 +24,116 @@
                 <h1>Sobre nós</h1>
             </div>
         </div>
-        <div class="introducao-abt container-abt">
-            <div class="title1">
-                <h2>Introdução</h2>
-            </div>
-            <div class="texto1">
+        <div class="introducao-abt container-abt">                    
+            <div class="texto1 textos">
+                <h2 class="title1">Introdução</h2>
                 <h3>Antes do surgimento da internet e do mundo se tornar globalizado, anúncios e promoções eram anunciadas via panfletos ou comunicação oral, não se aplicando ao contexto atual, onde temos um mundo globalizado e conectado.</h3>
                 <h3>O uso dos panfletos em grandes cidades necessitam de uma gigante quantidade de folhas papel, e caso a loja indique alguma informação errada é necessário que os panfletos sejam refeitos para não enfrentar problemas judiciais devido a uma propaganda “enganosa”, assim aumentando o custo do serviço, gerando ainda mais lixo e atrasando a distribuição dos panfletos, além do desmatamento dobrado para produzir essa quantidade enorme de papéis.</h3>
                 <h3>A execução desse projeto vem com o intuito de minimizar tal inconveniência, direcionando as atuais promoções para os consumidores interessados, e aos proprietários de lojas que desejam anunciar seus produtos de maneira digital e moderna.</h3>
             </div>
-            <div class="img1">
-
-            </div>
+            <div class="img1"></div>
         </div>
         <div class="objetivos-abt container-abt">
-            <div class="title2">
-                <h2>Objetivos</h2>
+            <div class="img2"></div>
+            <div class="texto2 textos">
+                <h2 class="title2">Objetivos</h2>
+                <h3>O objetivo principal do projeto é desenvolver um sistema web que anuncie aos consumidores as promoções de determinados produtos, tais como, elétricos, produtos alimentícios, vestuário etc. Não é a proposta da ferramenta, neste momento, permitir a venda ou compra de produtos. A ideia em si é anunciar os produtos que estejam em promoção, desde a pequena empresa até as gigantescas multinacionais.</h3>
+                <h3>Para a realização do nosso sistema, é necessário o levantamento de certos objetivos específicos, tais como:</h3>
+                <ul>
+                    <li><h3>Levantamento das informações de trabalhos realizados anteriormente;</h3></li>
+                    <li><h3>Levantamento dos requisitos do sistema;</h3></li>
+                    <li><h3>Desenvolvimento dos diagramas necessários;</h3></li>
+                    <li><h3>Escolha das linguagens adequadas para a construção do sistema;</h3></li>
+                    <li><h3>Modelamento dos dados;</h3></li>
+                    <li><h3>Criação do banco de dados;</h3></li>
+                    <li><h3>Implementação dos recursos obtidos nas etapas anteriores;</h3></li>
+                    <li><h3>Testes do sistema;</h3></li>
+                    <li><h3>Validação do projeto.</h3></li>
+                </ul>
             </div>
-            <div class="texto2">
-            <ul>
-                <li class="jose">O objetivo principal do projeto é desenvolver um sistema web que anuncie aos consumidores as promoções de determinados produtos, tais como, elétricos, produtos alimentícios, vestuário etc. Não é a proposta da ferramenta, neste momento, permitir a venda ou compra de produtos. A ideia em si é anunciar os produtos que estejam em promoção, desde a pequena empresa até as gigantescas multinacionais.</li>
-                <li class="jose">Para a realização do nosso sistema, é necessário o levantamento de certos objetivos específicos, tais como:
-                    <ul>
-                        <li class="jose2">Levantamento das informações de trabalhos realizados anteriormente;</li>
-                        <li>Levantamento dos requisitos do sistema;</li>
-                        <li>Desenvolvimento dos diagramas necessários;</li>
-                        <li>Escolha das linguagens adequadas para a construção do sistema;</li>
-                        <li>Modelamento dos dados;</li>
-                        <li>Criação do banco de dados;</li>
-                        <li>Implementação dos recursos obtidos nas etapas anteriores;</li>
-                        <li>Testes do sistema;</li>
-                        <li>Validação do projeto.</li>
-                    </ul>
-                </li>
-            </ul>
-            </div>
-            <div class="img2">
+        </div>
+        <div class="programadores-abt container-abt">
+            <h2 class="title3">Programadores</h2>
+            <div class="texto3 textos">
                 
+                <div class="texto30">
+                    <div class="programmer programmer1">
+                        <div class="programmer-images img-programmer1" id="img1">
+                        </div>
+                        <div class="programmer-text text-programmer1">
+                            <div class="programmer-name name-programmer1" id="name1">
+                            </div>
+                            <div class="programmer-disc disc-programmer1" id="bio1"></div>
+                            <div class="social-media social-media-programmer1">
+                                <a href="https://github.com/d1ng0ls" target="_blank"><img src="../../media/github.png" alt="github" title="github"></a>
+                                <a href="https://instagram.com/d1ng0ls" target="_blank"><img src="../../media/instagram.png" alt="instagram" title="instagram"></a>
+                                <a href="https://twitter.com/d1ng0ls" target="_blank"><img src="../../media/twitter.png" alt="twitter" title="twitter"></a>
+                                <a href="https://youtube.com/channel/UC0QHlFhQwxf-ZhrMa28wPiw" target="_blank"><img src="../../media/youtube.png" alt="youtube" title="youtube"></a>
+                                <a href="https://twitch.com/d1ng0ls" target="_blank"><img src="../../media/twitch.png" alt="twitch" title="twitch"></a>
+                                <a href="https://steamcommunity.com/id/d1ng0ls" target="_blank"><img src="../../media/steam.png" alt="steam" title="steam"></a>
+                                <a href="https://reddit.com/user/d1ng0ls" target="_blank"><img src="../../media/reddit.png" alt="reddit" title="reddit"></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="programmer programmer2">
+                        <div class="programmer-images img-programmer2" id="img2">
+                        </div>
+                        <div class="programmer-text text-programmer2">
+                            <div class="programmer-name name-programmer2" id="name2">
+                            </div>
+                            <div class="programmer-disc disc-programmer2" id="bio2"></div>
+                            <div class="social-media social-media-programmer2">
+                                <a href="https://github.com/carlosXSU" target="_blank"><img src="../../media/github.png" alt="github" title="github"></a>
+                                <a href="https://www.instagram.com/computer_video_002/" target="_blank"><img src="../../media/instagram.png" alt="instagram" title="instagram"></a>
+                                <a href="https://twitter.com/N4XSU" target="_blank"><img src="../../media/twitter.png" alt="twitter" title="twitter"></a>
+                                <a href="https://www.youtube.com/channel/UCkI_1_BFp3I8QeYUyiQRPWg" target="_blank"><img src="../../media/youtube.png" alt="youtube" title="youtube"></a>
+                                <a href="https://www.twitch.tv/andrenashii" target="_blank"><img src="../../media/twitch.png" alt="twitch" title="twitch"></a>
+                                <a href="https://steamcommunity.com/id/canabarra" target="_blank"><img src="../../media/steam.png" alt="steam" title="steam"></a>
+                                <a href="https://soundcloud.com/n4xsu" target="_blank"><img src="../../media/soundcloud.png" alt="soundcloud" title="soundcloud"></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="programmer programmer3">
+                        <div class="programmer-images img-programmer3" id="img3">
+                        </div>
+                        <div class="programmer-text text-programmer3">
+                            <div class="programmer-name name-programmer3" id="name3">
+                            </div>
+                            <div class="programmer-disc disc-programmer3" id="bio3"></div>
+                            <div class="social-media social-media-programmer3">
+                                <img src="../../media/instagram.png" alt="instagram">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="programmer programmer4">
+                        <div class="programmer-images img-programmer4" id="img4">
+                        </div>
+                        <div class="programmer-text text-programmer4">
+                            <div class="programmer-name name-programmer4" id="name4">
+                            </div>
+                            <div class="programmer-disc disc-programmer4" id="bio4"></div>
+                            <div class="social-media social-media-programmer4">
+                                <img src="../../media/instagram.png" alt="instagram">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="programmer programmer5">
+                        <div class="programmer-images img-programmer5" id="img5">
+                        </div>
+                        <div class="programmer-text text-programmer5">
+                            <div class="programmer-name name-programmer5" id="name5">
+                            </div>
+                            <div class="programmer-disc disc-programmer5" id="bio5"></div>
+                            <div class="social-media social-media-programmer5">
+                                <img src="../../media/instagram.png" alt="instagram">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="img30">
+                </div>
             </div>
+            
         </div>
     </div>
 
---
+++ includes/githubapi.js
@@ -0,0 +1,99 @@
+function github() {
+    var request = new XMLHttpRequest()
+
+    request.open('GET', 'https://api.github.com/users/d1ng0ls' , true)
+
+    request.onload = function() {
+        var data = JSON.parse(this.response);
+        
+        var name = data.name;
+        var bio = data.bio;
+        var img = data.avatar_url;
+
+        document.getElementById('name1').innerHTML = '<h2>' + name + '</h2>';
+        document.getElementById('bio1').innerHTML = '<p>"' + bio + '"</p>';
+        document.getElementById('img1').innerHTML = '<img src="'+img+'">'
+    }
+
+    request.send();
+}
+
+function github2() {
+    var request = new XMLHttpRequest()
+
+    request.open('GET', 'https://api.github.com/users/carlosxsu', true)
+
+    request.onload = function() {
+        var data = JSON.parse(this.response);
+        
+        var name = data.name;
+        var bio = data.bio;
+        var img = data.avatar_url;
+
+        document.getElementById('name2').innerHTML = '<h2>' + name + '</h2>';
+        document.getElementById('bio2').innerHTML = '<p>"' + bio + '"</p>';
+        document.getElementById('img2').innerHTML = '<img src="'+img+'">'
+    }
+
+    request.send();
+}
+
+function github3() {
+    var request = new XMLHttpRequest()
+
+    request.open('GET', 'https://api.github.com/users/boninii', true)
+
+    request.onload = function() {
+        var data = JSON.parse(this.response);
+        
+        var name = data.name;
+        var bio = data.bio;
+        var img = data.avatar_url;
+
+        document.getElementById('name3').innerHTML = '<h2>' + name + '</h2>';
+        document.getElementById('bio3').innerHTML = '<p>"' + bio + '"</p>';
+        document.getElementById('img3').innerHTML = '<img src="'+img+'">'
+    }
+
+    request.send();
+}
+
+function github4() {
+    var request = new XMLHttpRequest()
+
+    request.open('GET', 'https://api.github.com/users/Larbos-Naul', true)
+
+    request.onload = function() {
+        var data = JSON.parse(this.response);
+        
+        var name = data.name;
+        var bio = data.bio;
+        var img = data.avatar_url;
+
+        document.getElementById('name4').innerHTML = '<h2>' + name + '</h2>';
+        document.getElementById('bio4').innerHTML = '<p>"' + bio + '"</p>';
+        document.getElementById('img4').innerHTML = '<img src="'+img+'">'
+    }
+
+    request.send();
+}
+
+function github5() {
+    var request = new XMLHttpRequest()
+
+    request.open('GET', 'https://api.github.com/users/RenanCastrov', true)
+
+    request.onload = function() {
+        var data = JSON.parse(this.response);
+        
+        var name = data.name;
+        var bio = data.bio;
+        var img = data.avatar_url;
+
+        document.getElementById('name5').innerHTML = '<h2>' + name + '</h2>';
+        document.getElementById('bio5').innerHTML = '<p>"' + bio + '"</p>';
+        document.getElementById('img5').innerHTML = '<img src="'+img+'">'
+    }
+
+    request.send();
+}
