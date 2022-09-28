3631e864276de42f41ecc7e4529cad9f86ad381e
d1ng0ls <4b4c4t3666@gmail.com>
2022-09-28 10:41:01 -0300

    eduardo

--- comunidade/index.php
+++ comunidade/index.php
@@ -20,17 +20,7 @@
         <img src="comunidade-img\comecom-comunidade-logo2.png" id="comecom-logo">
     </div>
 
-    <div class="container1">  <!-- BOAS VINDAS -->
-        <div class="container1-1">
-            <p>Bem vindo ao fórum de conversas da COMECOM!</p>
-        </div>
-        <div class="container1-2">
-            <p>Olá cidadão, seja bem vindo ao fórum da COMECOM; com uma simples conta em nosso website, você poderá postar/discutir com
-                    outras pessoas do fórum sobre promoções que você viu ou sobre algum outro assunto relacionado a isso. Logo abaixo, você 
-                    pode acessar as regras do site. Caso você queira saber mais sobre a gente, considere acessar o FAQ também!  
-            </p>
-            <p>Acesse as <a href="rules.html">regras</a> do fórum e evite probelmas. Saiba mais sobre a gente no <a href="/">FAQ</a> caso você desejar! :)</p>
-        </div>
+    
     </div>
 
 <hr>
