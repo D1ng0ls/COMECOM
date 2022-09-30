936e70413d914323915a0008c55d995570fbd54d
luan <luangabrielsobral160804@gmail.com>
2022-09-30 10:12:13 -0300

    luan

--- comunidade/faq.php
+++ comunidade/faq.php
@@ -1,3 +1,5 @@
+<?php include('../includes/settings.php'); ?>
+
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
@@ -6,70 +8,61 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../style/style.css">
     <link rel="stylesheet" href="../style/style-navegador.css">
-    <link rel="stylesheet" href="../style/style-comunidade.css">
-    <title>COMECOM | FAQ</title>
-    <?php include('../includes/settings.php'); ?>
+    <link rel="stylesheet" href="../style/style-faq.css">
+    <title>COMECOM | Comunidade</title>
 </head>
 
+<script src="../scripts/githubapi.js"></script>
+<script>github();github2();github3();github4();github5();</script>
+
 <body>
-    <?php include('../includes/navigator.php'); ?>
-
-    <!-- <div class="container1">
-        <div class="container1-1">
-            <p>POSSO FAZER COMPRAS NA COMECOM?</p>
-        </div>
-        <div class="container1-2">
-            <p>Não, nosso objetivo é apenas a divulgação de promoções ativas por parte das vendedoras destes produtos, incentivando o cliente a comprá-los
-            em suas respectivas lojas físicas ou virtuais. </p>
-        </div>
+    <div class="comunidade">
+        <?php include('../includes/navigator.php'); ?>
     </div>
-
-    <div class="container1">
-        <div class="container1-1">
-            <p>POSSO VENDER UM PRODUTO NA COMECOM?</p>
+    <div class="conteudo">
+        <div class="logo-abt">
+            <div class="title-logo-abt">
+                <h1>FAQ - Frequently Asked Questions</h1>
+            </div>
         </div>
-        <div class="container1-2">
-            <p>Não. A página é voltada apenas para a divulgação de promoções ativas e não na venda dos produtos anunciados. 
-            </p>
+        <div class="introducao-abt container-abt">                    
+            <div class="texto1 textos">
+                <h2 class="title1">Posso fazer compras na COMECOM?</h2>
+                <h3>Não, nosso objetivo é apenas a divulgação de promoções ativas por parte das vendedoras destes produtos, incentivando o cliente a comprá-los
+            em suas respectivas lojas físicas ou virtuais.</h3>
+            </div>
+            <div class="img1"></div>
         </div>
-    </div>
-
-    <div class="container1">
-        <div class="container1-1">
-            <p>TODOS PODEM ANUNCIAR NA COMECOM?</p>
+        <div class="objetivos-abt container-abt">
+            <div class="img2"></div>
+            <div class="texto1 textos">
+                <h2 class="title1" style="border-bottom: 1px solid white;">Posso vender um produto na COMECOM?</h2>
+                <h3>Não. A página é voltada apenas para a divulgação de promoções ativas e não na venda dos produtos anunciados. </h3>
+            </div>
         </div>
-        <div class="container1-2">
-            <p>Sim, no entanto, anúncios realizados por pessoas físicas são destinados à aba da comunidade e os realizados por pessoas jurídicas/empresas são divulgados
-            em suas respectivas categorias. Isso para a maior segurança possível da veracidade dos anúncios exibidos na página.</p>
+        <div class="introducao-abt container-abt">                    
+            <div class="texto1 textos">
+                <h2 class="title1">Todos podem anunciar na COMECOM?</h2>
+                <h3>Sim, no entanto, anúncios realizados por pessoas físicas são destinados à aba da comunidade e os realizados por pessoas jurídicas/empresas são divulgados
+            em suas respectivas categorias. Isso para a maior segurança possível da veracidade dos anúncios exibidos na página.</h3>
+            </div>
+            <div class="img1"></div>
         </div>
-    </div>
-
-    <div class="container1">
-        <div class="container1-1">
-            <p>PERGUNTA 4</p>
+        <div class="objetivos-abt container-abt">
+            <div class="img2"></div>
+            <div class="texto1 textos">
+                <h2 class="title1"  style="border-bottom: 1px solid white;">Preciso estar cadastrado para anunciar na COMECOM?</h2>
+                <h3>Sim. Para a melhor veracidade das promoções apresentadas, solicitamos os dados específicos do usuário para distinguir usuários comuns de empresas e,
+                     consequentemente, fazermos a devida distinção dos respectivos anúncios produzidos por cada um. </h3>
+            </div>
         </div>
-        <div class="container1-2">
-            <p>RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA 
-            RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA 
-            RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA
-            RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA
-            </p>
+        <div class="introducao-abt container-abt">                    
+            <div class="texto1 textos">
+                <h2 class="title1">Preciso estar cadastrado para navegar na COMECOM?</h2>
+                <h3>Não. Qualquer visitante da página pode visualizar as promoções ativas, apenas será restrito da publicação destas por questões de segurança e controle da veracidade de informações anteriormente citadas.</h3>
+            </div>
+            <div class="img1"></div>
         </div>
-    </div>
-
-    <div class="container1">
-        <div class="container1-1">
-            <p>PERGUNTA 5</p>
-        </div>
-        <div class="container1-2">
-            <p>RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA 
-            RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA 
-            RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA
-            RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA RESPOSTA
-            </p>
-        </div>
-    </div> -->
-
     <?php include('../includes/footer.php'); ?>
 </body>
 
