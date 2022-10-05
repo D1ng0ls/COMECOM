199d48a1d69f5c45534f4d8d20de5512e11570f9
bonini <wakandadoceu@gmail.com>
2022-10-05 12:14:38 -0300

    bonini

--- usuarios.php
+++ usuarios.php
@@ -70,10 +70,12 @@
                             <tr>
                                 <td><?php echo $entidade['nome'] ?></td>
                                 <td><?php echo $entidade['email'] ?></td>
-                                <td><a href='core/usuario_repositorio.php?acao=status&id_pessoa=<?php echo $entidade['id_pessoa']?>&valor=<?php echo !$entidade['ativo']?>'><?php echo ($entidade['ativo']==1) ? 
-                                'Desativar' : 'Ativar'; ?></a></td>
-                                <td><a href='core/usuario_repositorio.php?acao=adm&id_pessoa=<?php echo $entidade['id_pessoa']?>&valor=<?php echo !$entidade['adm']?>'><?php echo ($entidade['adm']==1) ? 'Rebaixar'
-                                : 'Promover'; ?></a></td>
+                                <td><a href='core/usuario_repositorio.php?acao=status&id_pessoa=
+                                    <?php echo $entidade['id_pessoa']?>&valor=<?php echo !$entidade['ativo']?>'>
+                                    <?php echo ($entidade['ativo']==1) ? 'Desativar' : 'Ativar'; ?></a></td>
+                                <td><a href='core/usuario_repositorio.php?acao=adm&id_pessoa=
+                                <?php echo $entidade['id_pessoa']?>&valor=<?php echo !$entidade['adm']?>'>
+                                <?php echo ($entidade['adm']==1) ? 'Rebaixar' : 'Promover'; ?></a></td>
                             </tr>
                             <?php endforeach; ?>
                         </tbody>
