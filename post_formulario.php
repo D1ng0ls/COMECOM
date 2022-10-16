263d4d310c7dcf71e2e98a9941de8b2b6862d4f8
bonini <wakandadoceu@gmail.com>
2022-10-15 22:23:35 -0300

    bonini

--- post_formulario.php
+++ post_formulario.php
@@ -5,6 +5,7 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style/style-post-formulario.css">
         <title>Teste</title>
     </head>
     <body>
@@ -45,24 +46,24 @@
                             $entidade = $retorno[0];
                         }
                     ?>
-                    <h2>Post</h2>
                     <form method="post" action="core/post_repositorio.php">
                         <input  type="hidden" name="acao"
                                 value="<?php echo empty($id) ? 'insert' : 'update' ?>">
                         <input  type="hidden" name="id_publicacao"
                                 value="<?php echo $entidade['id_publicacao'] ?? '' ?>">
-                        <div>
-                            <label  for="titulo">Título</label>
-                            <input  type="text"
-                                    require="required" id="titulo" name="titulo"
-                                    value="<?php echo $entidade['titulo'] ?? ''?>">
-                        </div>
-                        <div>
-                            <label for="texto">Texto</label>
+                        <div class="container1">
+                            <div class="post-postar"><h2>Postar</h2></div>
+                            <div class="post-titulo">
+                                <input  type="text"
+                                        require="required" id="titulo" name="titulo" placeholder="Título"
+                                        value="<?php echo $entidade['titulo'] ?? ''?>">
+                            </div>
+                            <div class="post-textarea">
 <textarea type="text"
-require="required" id="texto" name="texto" rows="5">
+require="required" id="texto" name="texto" rows="5" placeholder="Texto (opcional)">
 <?php echo $entidade['texto'] ?? ''?>
 </textarea>
+                            </div>
                         </div>
                         <div>
                             <button type="submit">Salvar</button>
