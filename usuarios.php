2be43c5bb42df8b2d8e11636d3e81f88c6f09250
bonini <wakandadoceu@gmail.com>
2022-10-21 10:58:01 -0300

    bonini

--- usuarios.php
+++ usuarios.php
@@ -1,81 +1,102 @@
 <!DOCTYPE html>
 <html lang="pt_BR">
     <head>
-    <meta charset="UTF-8">
+        <?php include('includes/settings.php'); ?>
+        <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="style/style-usuarios.css">
+        <link rel="stylesheet" href="style/style.css">
+        <link rel="stylesheet" href="style/style-navegador.css">
+        <link rel="stylesheet" href="style/style-busca.css">
         <title>COMECOM | ADM</title>
     </head>
     <body>
-        <?php
-            session_start();
-            include 'includes/valida_login.php';
-            if($_SESSION['login']['pessoa']['adm'] !== 1) {
-                header('Location: ../COMECOM');
-            }
-        ?>
-        <h2>Usuário</h2>
-        <?php include 'includes/busca.php' ?>
-        <?php
-            require_once 'includes/funcoes.php';
-            require_once 'core/conexao_mysql.php';
-            require_once 'core/sql.php';
-            require_once 'core/mysql.php';
+        <div>
+            <?php
+                include 'includes/navigator.php';
+                include 'includes/valida_login.php';
+                if($_SESSION['login']['pessoa']['adm'] !== 1) {
+                    header('Location: ../COMECOM');
+                }
+            ?>
+        </div>
+        <div class="container-users">
+            <div class="title-page">
+                <div class="container-title-page-1">
+                    Administração
+                </div>
+                <div class="container-title-page-2">
+                    Gerenciamento de usuários
+                </div>
+            </div>
+            <?php include 'includes/busca.php' ?>
+            <?php
+                require_once 'includes/funcoes.php';
+                require_once 'core/conexao_mysql.php';
+                require_once 'core/sql.php';
+                require_once 'core/mysql.php';
 
-            foreach($_GET as $indice => $dado) {
-                $$indice = limparDados($dado);
-            }
+                foreach($_GET as $indice => $dado) {
+                    $$indice = limparDados($dado);
+                }
 
-            $data_atual = date('Y-m-d H:i:s');
+                $data_atual = date('Y-m-d H:i:s');
 
-            $criterio = [];
+                $criterio = [];
 
-            if(!empty($busca)) {
-                $criterio [] = ['nome', 'like', "%{$busca}%"];
-            }
+                if(!empty($busca)) {
+                    $criterio [] = ['nome', 'like', "%{$busca}%"];
+                }
 
-            $result = buscar(
-                'pessoa',
-                [
-                    'id_pessoa',
-                    'nome',
-                    'email',
-                    'data_criacao',
-                    'ativo',
-                    'adm'
-                ],
-                $criterio,
-                'data_criacao DESC, nome ASC'
-            );
+                $result = buscar(
+                    'pessoa',
+                    [
+                        'id_pessoa',
+                        'nome',
+                        'email',
+                        'data_criacao',
+                        'ativo',
+                        'adm'
+                    ],
+                    $criterio,
+                    'data_criacao DESC, nome ASC'
+                );
 
-        ?>
-        <table>
-            <thead>
-                <tr>
-                    <td>Nome</td>
-                    <td>E-mail</td>
-                    <td>Data criacao</td>
-                    <td>Ativo</td>
-                    <td>Administrador</td>
-                </tr>
-            </thead>
-            <tbody>
+            ?>
+            <div class="table-users">
+                <table>
+                    <thead>
+                        <tr>
+                            <td>Nome</td>
+                            <td>E-mail</td>
+                            <td>Data criacao</td>
+                            <td>Ativo</td>
+                            <td>Administrador</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                            foreach($result as $entidade):
+                                $data = date_create($entidade['data_criacao']);
+                                $data = date_format($data, 'd/m/Y H:i:s');
+                        ?>
+                        <tr>
+                            <td><?php echo $entidade['nome'] ?></td>
+                            <td><?php echo $entidade['email'] ?></td>
+                            <td><?php echo $data ?></td>
+                            <td><a href="core/usuario_repositorio.php?acao=status&id_pessoa=<?php echo $entidade['id_pessoa']?>&valor=<?php echo !$entidade['ativo']?>"><?php echo ($entidade['ativo']==1) ? 'Desativar' : 'Ativar'; ?></a></td>
+                            <td><a href="core/usuario_repositorio.php?acao=adm&id_pessoa=<?php echo $entidade['id_pessoa']?>&valor=<?php echo !$entidade['adm']?>"><?php echo ($entidade['adm']==1) ? 'Rebaixar' : 'Promover'; ?></a></td>
+                        </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table> 
+            </div>
+        </div>
+        <div>
                 <?php
-                    foreach($result as $entidade):
-                        $data = date_create($entidade['data_criacao']);
-                        $data = date_format($data, 'd/m/Y H:i:s');
+                    include 'includes/footer.php';
                 ?>
-                <tr>
-                    <td><?php echo $entidade['nome'] ?></td>
-                    <td><?php echo $entidade['email'] ?></td>
-                    <td><?php echo $data ?></td>
-                    <td><a href="core/usuario_repositorio.php?acao=status&id_pessoa=<?php echo $entidade['id_pessoa']?>&valor=<?php echo !$entidade['ativo']?>"><?php echo ($entidade['ativo']==1) ? 'Desativar' : 'Ativar'; ?></a></td>
-                    <td><a href="core/usuario_repositorio.php?acao=adm&id_pessoa=<?php echo $entidade['id_pessoa']?>&valor=<?php echo !$entidade['adm']?>"><?php echo ($entidade['adm']==1) ? 'Rebaixar' : 'Promover'; ?></a></td>
-                </tr>
-                <?php endforeach; ?>
-            </tbody>
-        </table>  
-        <a href="../COMECOM">Vortar</a>  
+            </div>
     </body>
 </html>
