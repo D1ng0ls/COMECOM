4daedf003a9ab0a79754aba73e6d028b04f26896
bonini <wakandadoceu@gmail.com>
2022-11-18 11:01:08 -0300

    bonini

--- core/usuario_repositorio.php
+++ core/usuario_repositorio.php
@@ -105,7 +105,9 @@
                     $foto_name = $_FILES['foto']['name'][$i];
                     $path_parts = pathinfo($foto_name);
                     $imageFileType = strtolower(pathinfo($foto_name, PATHINFO_EXTENSION));
-                    $foto_name = $path_parts['filename'].time().".".$imageFileType;
+                    // $foto_name = $path_parts['filename'].time().".".$imageFileType;
+                    // $newFilePath = "../upload/user/dilma.jpg"; 
+                    $foto_name = $id_pessoa . "_" . $nome . "." . $imageFileType;
                     $newFilePath = "../upload/user/" . $foto_name;
                     if(move_uploaded_file($tmpFilePath, $newFilePath)) {
                         $fotos_name[] = $foto_name;
